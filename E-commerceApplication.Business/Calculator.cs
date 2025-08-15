cd110a384b0f8f57bc5e07ce1ea6dedcd70e1292
Kolyo Stoyanov Kolev <kolyokolev301@gmail.com>
2025-08-15 20:37:02 +0300

    test: setup xUnit with FakeItEasy and AutoFixture; add Calculate class in Business layer and Calculations test class in xUnit for assert verification.

---
+++ E-commerceApplication.Business/Calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace E_commerceApplication.Business
+{
+    public class Calculator
+    {
+        public Calculator() { }
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
