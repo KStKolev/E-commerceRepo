765fbf3e1050b9c388aaed3f141befee80e0a965
Kolyo Stoyanov Kolev <kolyokolev301@gmail.com>
2025-08-15 15:01:15 +0300

    chore: initial setup for e-commerce application (web, business, DAL layers)

---
+++ E-commerceApplication/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerceApplication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet()]
+        public string GetInfo()
+        {
+            return "Hello world";
+        }
+    }
+}
