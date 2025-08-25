6ad84b0b34d7117f8406c78e0d2a5f46a36c8574
Kolyo Stoyanov Kolev <kolyokolev301@gmail.com>
2025-08-25 21:15:48 +0300

    feat(auth): add authentication and email services with request models
    - Created IAuthService for user authentication and IEmailService for email confirmation
    - Added three request models: AuthCredentialsRequestModel, SignInRequestModel, SignUpRequestModel
    - Implemented AuthService and EmailService using the respective interfaces
    - Added SmtpSettings class to provide configuration for SmtpClient in email confirmation

---
+++ E-commerceApplication.Business/Models/AuthCredentialsRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace E_commerceApplication.Business.Models
+{
+    public class AuthCredentialsRequestModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ E-commerceApplication.Business/Services/EmailService.cs
@@ -0,0 +1,37 @@
+﻿using E_commerceApplication.Business.Interfaces;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+
+namespace E_commerceApplication.Business.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly SmtpSettings _smtpSettings;
+
+        public EmailService(IOptions<SmtpSettings> smtpSettings)
+        {
+            _smtpSettings = smtpSettings.Value;
+        }
+
+        public async Task SendEmailConfirmationAsync(string toEmail, string link)
+        {
+            using SmtpClient client = new(_smtpSettings.Host, _smtpSettings.Port)
+            {
+                Credentials = new NetworkCredential(_smtpSettings.User, _smtpSettings.Password),
+                EnableSsl = true
+            };
+
+            var mail = new MailMessage
+            {
+                From = new MailAddress(_smtpSettings.User, "E-commerceApplication"),
+                Subject = "Confirm your email",
+                Body = $"Please confirm your account by clicking this link: <a href='{link}'>Confirm Email</a>",
+                IsBodyHtml = true
+            };
+
+            mail.To.Add(toEmail);
+            await client.SendMailAsync(mail);
+        }
+    }
+}
