66cfcacb97ffed827783f877766cceac54db91e0
Kolyo Stoyanov Kolev <kolyokolev301@gmail.com>
2025-08-27 21:17:50 +0300

    feat: add resource files and validation patterns
    - Added resource files for email content, exception messages, and validation messages
    - Created a static ValidationPatterns class containing regex patterns

---
+++ E-commerceApplication/Validation/ValidationPatterns.cs
@@ -0,0 +1,9 @@
+﻿namespace E_commerceApplication.Validation
+{
+    public static class ValidationPatterns
+    {
+        public const string EmailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+        public const string PasswordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+        public const string PhoneNumberPattern = @"^(?=(?:\D*\d){7,15}\D*$)\+?[\d\s().-]+$";
+    }
+}
