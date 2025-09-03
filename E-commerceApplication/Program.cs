a8cf461d8df7d8c1a540b3a7572468087cd5a00b
Kolyo Stoyanov Kolev <kolyokolev301@gmail.com>
2025-09-03 13:55:59 +0300

    docs(swagger): add XML documentation and used in AuthController, UserController, and GamesController

--- E-commerceApplication/Program.cs
+++ E-commerceApplication/Program.cs
@@ -29,7 +29,12 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddApplicationServices();
 
