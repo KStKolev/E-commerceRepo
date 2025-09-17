729fd2025d6c493b7d8e6c3805c5fb38ab4e57cc
Kolyo Stoyanov Kolev <kolyokolev301@gmail.com>
2025-09-17 16:49:44 +0300

    test(rating): add EditRatingDtoTests for validation checks
    - Created EditRatingDtoTests in E_commerceApplication.Tests.DTOTests
    - Added Rating_InvalidInput_ShouldFailValidation to verify invalid ratings trigger validation errors

---
+++ E-commerceApplication.Tests/DTOTests/EditRatingDtoTests.cs
@@ -0,0 +1,26 @@
+﻿using E_commerceApplication.DTOs;
+
+namespace E_commerceApplication.Tests.DTOTests
+{
+    public class EditRatingDtoTests
+    {
+        [Fact]
+        public void Rating_InvalidInput_ShouldFailValidation() 
+        {
+            var dto = new EditRatingRequestDto() 
+            {
+                ProductId = 2,
+                Rating = 7
+            };
+
+            var results = ValidationHelper
+              .ValidateModel(dto);
+
+            Assert
+             .Single(results);
+
+            Assert
+                .Contains(results, r => r.MemberNames.Contains(nameof(EditRatingRequestDto.Rating)));
+        }
+    }
+}
