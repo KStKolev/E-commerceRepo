3800e600881e7cbd113190b2f2f0b1f4ea5b1953
Kolyo Kolev <114016354+KStKolev@users.noreply.github.com>
2025-09-23 21:21:39 +0300

    feat(cache): add caching services and key prefixes
    - Created ICacheService for caching functionality
    - Injected ICacheService into IUserCacheService
    - Added CacheKeyPrefixes static class for managing cache key prefixes
    - Registered caching services in ServiceCollectionExtension

---
+++ E-commerceApplication.Business/Interfaces/ICacheService.cs
@@ -0,0 +1,11 @@
+﻿namespace E_commerceApplication.Business.Interfaces
+{
+    public interface ICacheService
+    {
+        T? Get<T>(string key);
+
+        void Set<T>(string key, T value, TimeSpan expiration);
+
+        void Remove(string key);
+    }
+}
---
+++ E-commerceApplication.Business/Interfaces/IUserCacheService.cs
@@ -0,0 +1,11 @@
+﻿namespace E_commerceApplication.Business.Interfaces
+{
+    public interface IUserCacheService
+    {
+        T? Get<T>(string userId);
+
+        void Set<T>(string userId, T value, TimeSpan? absoluteExpiration = null);
+
+        void Remove(string userId);
+    }
+}
